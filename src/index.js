ddf13a55deac7f379407de96d7b49a1ff65af1ba
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-03-29 17:26:09 +0100

    Created temporarily or not this branch after getting some problems with webpack and the whole situation of showing images.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import "./styles/style.css";
-import "./styles/bestProjects.css";
-
-import html from "./index.html";
-import html2 from "./bestProjects.html";
-
-import {} from "./scripts/darkLightMode";
