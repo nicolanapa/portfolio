8564d92a5a78ec0751115304a0910b69a53eabc6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-25 10:41:03 +0200

    Added some code that checks if the darkLightMode localStorage item is already checked and another that changes the modes when a button is clicked

--- dist/scripts/darkLightMode.js
+++ dist/scripts/darkLightMode.js
@@ -1,3 +1,23 @@
 // Use localStorage to keep user preference
 // 0 light 1 dark
 console.log("darkLightMode.js test");
+
+if (localStorage.getItem("darkLightMode") === "light") {
+    console.log("LIGHT MODE AT START");
+} else if (localStorage.getItem("darkLightMode") === "dark") {
+    console.log("DARK MODE AT START");
+} else if (localStorage.getItem("darkLightMode") === "") {
+    console.log("DEFAULT MODE AT START");
+    localStorage.setItem("darkLightMode", "light");
+}
+
+const darkLightModeButton = document.querySelector(".dark-light-mode");
+
+darkLightModeButton.addEventListener("click", () => {
+    console.log("clicked!");
+    if (localStorage.getItem("darkLightMode") === "light") {
+        localStorage.setItem("darkLightMode", "dark");
+    } else if (localStorage.getItem("darkLightMode") === "dark") {
+        localStorage.setItem("darkLightMode", "light");
+    }
+});
