ed175ca7e2c1d1e9d7815b542d225c86e636fe32
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-25 10:52:32 +0200

    Added toLightMode() and toDarkMode()) which will change the themeLink href

--- dist/scripts/darkLightMode.js
+++ dist/scripts/darkLightMode.js
@@ -1,14 +1,27 @@
 // Use localStorage to keep user preference
-// 0 light 1 dark
+
+const themeLink = document.querySelector("#theme-mode");
+
+function toLightMode() {
+    console.log("SWITCHING TO LIGHT MODE");
+}
+
+function toDarkMode() {
+    console.log("SWITCHING TO DARK MODE");
+}
+
 console.log("darkLightMode.js test");
 
 if (localStorage.getItem("darkLightMode") === "light") {
     console.log("LIGHT MODE AT START");
+    themeLink.href = "./styles/lightMode.css";
 } else if (localStorage.getItem("darkLightMode") === "dark") {
     console.log("DARK MODE AT START");
+    themeLink.href = "./styles/darkMode.css";
 } else if (localStorage.getItem("darkLightMode") === "") {
     console.log("DEFAULT MODE AT START");
     localStorage.setItem("darkLightMode", "light");
+    themeLink.href = "./styles/lightMode.css";
 }
 
 const darkLightModeButton = document.querySelector(".dark-light-mode");
@@ -17,7 +30,9 @@ darkLightModeButton.addEventListener("click", () => {
     console.log("clicked!");
     if (localStorage.getItem("darkLightMode") === "light") {
         localStorage.setItem("darkLightMode", "dark");
+        toDarkMode();
     } else if (localStorage.getItem("darkLightMode") === "dark") {
         localStorage.setItem("darkLightMode", "light");
+        toLightMode();
     }
 });
