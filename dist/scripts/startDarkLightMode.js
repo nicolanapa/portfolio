234ec1fbc92e2c014d5abfb85b503174fde04ae9
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-25 13:50:33 +0200

    Added bestProjects and projects linking

--- dist/scripts/startDarkLightMode.js
+++ dist/scripts/startDarkLightMode.js
@@ -1,12 +1,26 @@
 const themeLink = document.querySelector("#theme-mode");
 const aboutMeLink = document.querySelector("#about-me-mode");
+const bestProjectsLink = document.querySelector("#best-projects-mode");
+const projectsLink = document.querySelector("#projects-mode");
 
 function toLightMode() {
     console.log("SWITCHING TO LIGHT MODE");
 
-    themeLink.href = "./styles/lightMode/lightMode.css";
     if (aboutMeLink !== null) {
         aboutMeLink.href = "./styles/lightMode/aboutMe.css";
+    }
+    // When it's in the projects directory
+    if (projectsLink === null) {
+        themeLink.href = "./styles/lightMode/lightMode.css";
+        if (bestProjectsLink !== null) {
+            bestProjectsLink.href = "./styles/lightMode/bestProjects.css";
+        }
+    } else if (projectsLink !== null) {
+        themeLink.href = "../styles/lightMode/lightMode.css";
+        projectsLink.href = "../styles/lightMode/projects.css";
+        if (bestProjectsLink !== null) {
+            bestProjectsLink.href = "../styles/lightMode/bestProjects.css";
+        }
     }
 }
 
@@ -14,8 +28,15 @@ function toDarkMode() {
     console.log("SWITCHING TO DARK MODE");
 
     themeLink.href = "./styles/darkMode/darkMode.css";
+
     if (aboutMeLink !== null) {
         aboutMeLink.href = "./styles/darkMode/aboutMe.css";
+    }
+    if (bestProjectsLink !== null) {
+        bestProjectsLink.href = "./styles/lightMode/projects.css";
+    }
+    if (projectsLink !== null) {
+        projectsLink.href = "./styles/lightMode/projects.css";
     }
 }
 
