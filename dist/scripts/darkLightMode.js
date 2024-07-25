0442555aaf1e12fa9fd3008a19f176150c00b3f7
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-25 11:06:56 +0200

    Removed the two Functions and the loading of the theme automatically

--- dist/scripts/darkLightMode.js
+++ dist/scripts/darkLightMode.js
@@ -1,30 +1,8 @@
+import { toLightMode, toDarkMode } from "./startDarkLightMode.js";
+
 // Use localStorage to keep user preference
 
-const themeLink = document.querySelector("#theme-mode");
-
-function toLightMode() {
-    console.log("SWITCHING TO LIGHT MODE");
-    themeLink.href = "./styles/lightMode.css";
-}
-
-function toDarkMode() {
-    console.log("SWITCHING TO DARK MODE");
-    themeLink.href = "./styles/darkMode.css";
-}
-
 console.log("darkLightMode.js test");
-
-if (localStorage.getItem("darkLightMode") === "light") {
-    console.log("LIGHT MODE AT START");
-    toLightMode();
-} else if (localStorage.getItem("darkLightMode") === "dark") {
-    console.log("DARK MODE AT START");
-    toDarkMode();
-} else if (localStorage.getItem("darkLightMode") === "") {
-    console.log("DEFAULT MODE AT START");
-    localStorage.setItem("darkLightMode", "light");
-    toLightMode();
-}
 
 const darkLightModeButton = document.querySelector(".dark-light-mode");
 
