6700bff5568fd1bcdec6de2274ef4f3a442f2200
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-25 13:58:34 +0200

    Tried fixing clicking not working onmobile but it's not working

--- dist/scripts/darkLightMode.js
+++ dist/scripts/darkLightMode.js
@@ -6,8 +6,10 @@ console.log("darkLightMode.js test");
 
 const darkLightModeButton = document.querySelector(".dark-light-mode");
 
-darkLightModeButton.addEventListener("click", () => {
+darkLightModeButton.addEventListener("click", (Event) => {
+    Event.preventDefault();
     console.log("clicked!");
+
     if (localStorage.getItem("darkLightMode") === "light") {
         localStorage.setItem("darkLightMode", "dark");
         toDarkMode();
@@ -17,15 +19,19 @@ darkLightModeButton.addEventListener("click", () => {
     }
 });
 
-/*darkLightModeButton.addEventListener("touchmove", () => {
-    Event.preventDefault();
-    
-    console.log("clicked!");
-    if (localStorage.getItem("darkLightMode") === "light") {
-        localStorage.setItem("darkLightMode", "dark");
-        toDarkMode();
-    } else if (localStorage.getItem("darkLightMode") === "dark") {
-        localStorage.setItem("darkLightMode", "light");
-        toLightMode();
-    }
-});*/
+/*darkLightModeButton.addEventListener(
+    "touchstart",
+    (Event) => {
+        Event.preventDefault();
+        console.log("clicked mobile!");
+
+        if (localStorage.getItem("darkLightMode") === "light") {
+            localStorage.setItem("darkLightMode", "dark");
+            toDarkMode();
+        } else if (localStorage.getItem("darkLightMode") === "dark") {
+            localStorage.setItem("darkLightMode", "light");
+            toLightMode();
+        }
+    },
+    false,
+);*/
