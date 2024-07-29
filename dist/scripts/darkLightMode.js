c331561ef4ebfc36955ec3388d0034c1ff2112f4
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-29 17:57:27 +0200

    Removed and commented the console.log for testing the darkLightMode

--- dist/scripts/darkLightMode.js
+++ dist/scripts/darkLightMode.js
@@ -1,13 +1,9 @@
 import { toLightMode, toDarkMode } from "./startDarkLightMode.js";
 
-// Use localStorage to keep user preference
-
-console.log("darkLightMode.js test");
-
 const darkLightModeButton = document.querySelector(".dark-light-mode");
 
 darkLightModeButton.addEventListener("click", () => {
-    console.log("clicked!");
+    //console.log("clicked!");
 
     if (localStorage.getItem("darkLightMode") === "light") {
         localStorage.setItem("darkLightMode", "dark");
