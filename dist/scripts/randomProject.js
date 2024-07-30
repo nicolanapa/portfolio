45ce0f371b9f4f854984d09aa99b14bd60adc7e6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-07-30 11:07:03 +0200

    Added a loadRandomProject() Function which gets the current Subdomain and opens a random Project

---
+++ dist/scripts/randomProject.js
@@ -0,0 +1,21 @@
+function loadRandomProject() {
+    const allLinks = ["todoList", "weatherApp", "ticTacToe", "battleShip", "memoryCard"];
+    let randomNumber = Math.floor(Math.random() * allLinks.length);
+    let currentSubDomain = window.location.host.split(".")[0];
+
+    console.log(currentSubDomain);
+    alert("If nothing loads then check your popup settings");
+
+    window.open(
+        currentSubDomain === "nicolanapa"
+            ? "https://nicolanapa.duckdns.org/projects/"
+            : "https://development.nicolanapa.duckdns.org/projects/" + allLinks[randomNumber] + ".html",
+    );
+}
+
+const randomProjectButton = document.querySelectorAll(".random-project");
+
+for (let i = 0; i < 2; i++) {
+    randomProjectButton[i].addEventListener("click", loadRandomProject);
+}
+
